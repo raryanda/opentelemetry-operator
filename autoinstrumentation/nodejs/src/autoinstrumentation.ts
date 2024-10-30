e01378fb7c6677ae8d2387bb7f99b06bf6b91a8e
Randi Aryanda <randi.aryanda@tech.jago.com>
2024-10-30 15:16:56 +0700

    feat: add support for prom exporter to node 0.26.0

--- autoinstrumentation/nodejs/src/autoinstrumentation.ts
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -2,11 +2,20 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-otlp-grpc';
 
 import { NodeSDK } from '@opentelemetry/sdk-node';
+import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
+
+const getMetricExporter = () => {
+    if (process.env.OTEL_METRICS_EXPORTER === 'prometheus') {
+        return new PrometheusExporter();
+    }
+    return undefined;
+};
 
 const sdk = new NodeSDK({
     autoDetectResources: true,
     instrumentations: [getNodeAutoInstrumentations()],
     traceExporter: new OTLPTraceExporter(),
+    metricExporter: getMetricExporter()
 });
 
 sdk.start();
